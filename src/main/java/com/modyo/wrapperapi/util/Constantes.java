6e5944e65db316fdc6e82d102a232f2ee5ffcc58
Sergio Mena <sergio.mena@tecno.com.mx>
2023-02-13 09:31:55 -0600

    cambio de nombre de constante para usar en el front
    cambio de nombre de constante para usar en el front

--- src/main/java/com/modyo/wrapperapi/util/Constantes.java
+++ src/main/java/com/modyo/wrapperapi/util/Constantes.java
@@ -8,7 +8,7 @@ public class Constantes {
     public static final String MESSAGE= "message";
     public static final int ERROR_VALIDATION = -200;
     public static final String POKEMONS = "pokemons";
-    public static final String CURRENT_PAGE = "currentPage";
+    public static final String CURRENT_PAGE = "number";
     public static final String TOTAL_ELEMENTS = "totalElements";
     public static final String TOTAL_PAGES = "totalPages";
     public static final String NUMBER_OF_ELEMENTS = "numberOfElements";
